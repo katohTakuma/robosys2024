a7eed0745d6381a4b00539396d46967a82db52f6
Takuma Katou <s23c1037tg@s.chibakoudai.jp>
2024-11-26 18:53:07 +0900

    Add a command

--- test1.bash
+++ test1.bash
@@ -5,15 +5,15 @@
 input_text="hello world"
 expected_output="HELLO WORLD"
 
-# homework1.pyを実行し、標準入力に文字列を渡して標準出力を取得
+# homework1.pyを実行し、標準入力に文字を代入して標準出力を取得
 output=$(echo "$input_text" | python3 homework1.py)
 
-# 大文字変換された結果が期待通りかをチェック
+# 大文字変換された結果が期待通りかを確認
 if [ "$output" = "$expected_output" ]; then
-    echo "Test passed!"
+    echo "OK"
     exit 0
 else
-    echo "Test failed! Expected '$expected_output', but got '$output'"
+    echo "failed"
     exit 1
 fi
 
