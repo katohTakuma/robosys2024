d7cb53b60545dfbbf3aa8f612d34b5c20c11cd70
Takuma Katou <s23c1037tg@s.chibakoudai.jp>
2024-11-26 19:12:15 +0900

    Add a command

--- homework1.py
+++ homework1.py
@@ -5,14 +5,10 @@
 import sys
 
 def process_input(input_text):
-    # 入力されたテキストをすべて大文字に変換する処理
     return input_text.strip().upper()
 
 if __name__ == "__main__":
-    # 標準入力からデータを読み込む
-    input_text = sys.stdin.read()
-    # 入力を加工する
-    processed_text = process_input(input_text)
-    # 加工結果を標準出力に出力する
-    print(processed_text)
+        input_text = sys.stdin.read()
+        processed_text = process_input(input_text)
+        print(processed_text)
 
