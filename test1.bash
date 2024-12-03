731a73e20837f34c05bb75589e64557512e1bca5
Takuma Katou <s23c1037tg@s.chibakoudai.jp>
2024-12-03 13:02:04 +0900

    Add a command

--- test1.bash
+++ test1.bash
@@ -5,7 +5,7 @@
 input_text="hello world"
 expected_output="HELLO WORLD"
 
-output=$(echo "$input_text" | python3 homework1)
+output=$(echo "$input_text" | python3 OMOJI)
 
 if [ "$output" = "$expected_output" ]; then
     echo "OK"
