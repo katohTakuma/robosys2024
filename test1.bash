3341321b4dda30de2ad80d75d1da21b8a55d8a22
Takuma Katou <s23c1037tg@s.chibakoudai.jp>
2024-12-04 22:45:14 +0900

    Add a command

--- test1.bash
+++ test1.bash
@@ -2,7 +2,7 @@
 # SPDX-FileCopyrightText: 2024 katoh takuma
 # SPDX-License-Identifier: BSD-3-Clause
 
-# 1つ目のテスト：小文字を大文字に変換
+
 output=$(echo "hello world" | python3 OMOJI)
 if [ "$output" = "HELLO WORLD" ]; then
     echo "OK"
@@ -11,7 +11,7 @@ else
     exit 1
 fi
 
-# 2つ目のテスト：数字を含む場合
+
 output=$(echo "hello 123" | python3 OMOJI)
 if [ "$output" = "変換できません" ]; then
     echo "OK"
@@ -20,7 +20,7 @@ else
     exit 1
 fi
 
-# 3つ目のテスト：記号を含む場合
+
 output=$(echo "hello, world!" | python3 OMOJI)
 if [ "$output" = "変換できません" ]; then
     echo "OK"
@@ -29,7 +29,7 @@ else
     exit 1
 fi
 
-# 4つ目のテスト：ひらがなを含む場合
+
 output=$(echo "こんにちは" | python3 OMOJI)
 if [ "$output" = "変換できません" ]; then
     echo "OK"
@@ -38,7 +38,7 @@ else
     exit 1
 fi
 
-# 5つ目のテスト：英語とカタカナが混ざっている場合
+
 output=$(echo "Hello カタカナ" | python3 OMOJI)
 if [ "$output" = "変換できません" ]; then
     echo "OK"
