6ce50a524010d62c150d46db70d059117a2af753
Takuma Katou <s23c1037tg@s.chibakoudai.jp>
2024-12-04 09:35:20 +0900

    Add a command

--- test1.bash
+++ test1.bash
@@ -2,10 +2,17 @@
 # SPDX-FileCopyrightText: 2024 katoh takuma
 # SPDX-License-Identifier: BSD-3-Clause
 
-# 4つ目のテスト：ひらがなを含む場合
-output=$(echo "こんにちは" | python3 OMOJI)
-echo "OMOJI の出力: '$output'"  # OMOJI の出力を表示
+# 1つ目のテスト：小文字を大文字に変換
+output=$(echo "hello world" | python3 OMOJI)
+if [ "$output" = "HELLO WORLD" ]; then
+    echo "OK"
+else
+    echo "エラー"
+    exit 1
+fi
 
+# 2つ目のテスト：数字を含む場合
+output=$(echo "hello 123" | python3 OMOJI)
 if [ "$output" = "変換できません" ]; then
     echo "OK"
 else
@@ -13,3 +20,30 @@ else
     exit 1
 fi
 
+# 3つ目のテスト：記号を含む場合
+output=$(echo "hello, world!" | python3 OMOJI)
+if [ "$output" = "変換できません" ]; then
+    echo "OK"
+else
+    echo "エラー"
+    exit 1
+fi
+
+# 4つ目のテスト：ひらがなを含む場合
+output=$(echo "こんにちは" | python3 OMOJI)
+if [ "$output" = "変換できません" ]; then
+    echo "OK"
+else
+    echo "エラー"
+    exit 1
+fi
+
+# 5つ目のテスト：英語とカタカナが混ざっている場合
+output=$(echo "Hello カタカナ" | python3 OMOJI)
+if [ "$output" = "変換できません" ]; then
+    echo "OK"
+else
+    echo "エラー"
+    exit 1
+fi
+exit 0
